d3927db74ee70f033fe694202ce3287bd0327a42
Carlos Eduardo Montenegro <cmontenegroh@gmail.com>
2024-02-15 16:51:56 +0100

    reducer

---
+++ reducers/users.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: { token: null, username: null },
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.value.token = action.payload.token;
+      state.value.username = action.payload.username;
+    },
+    logout: (state) => {
+      state.value.token = null;
+      state.value.username = null;
+    },
+    signUp: (state, action) => {
+      state.value.push(action.payload);
+    },
+  },
+});
+
+export const { login, logout, signUp } = userSlice.actions;
+export default userSlice.reducer;
