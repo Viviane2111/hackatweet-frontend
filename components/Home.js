4140f832c9e992a0344d24af3d52544a70759f8a
Viviane2111 <schommersv06@gmail.com>
2024-02-15 12:25:11 +0100

    add onClick

--- components/Home.js
+++ components/Home.js
@@ -9,13 +9,13 @@ function Home() {
         <title>Hackatweet - Home</title>
       </Head>
       <main className={styles.main}>
-        <div className={styles.leftside}>
-
-        </div>
+        <div className={styles.leftside}></div>
         <div className={styles.rightside}>
           <h1 className={styles.title}>Create your Hackatweet account</h1>
           <h3>Join Hackatweet today</h3>
-          <button className={styles.btnLogs}>Sign up</button>
+          <button onClick={showModal()} className={styles.btnLogs}>
+            Sign up
+          </button>
           <p>Already have an account ?</p>
           <button className={styles.btnLogs}>Sign in</button>
         </div>
