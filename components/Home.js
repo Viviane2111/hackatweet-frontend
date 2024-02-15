0eb1a9e9edf40407cb318f147c7af94b6ed20b06
Viviane2111 <schommersv06@gmail.com>
2024-02-15 15:02:37 +0100

    custom modal

--- components/Home.js
+++ components/Home.js
@@ -2,6 +2,7 @@ import styles from "../styles/Home.module.css";
 import Modal from "react-modal";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 
 function Home() {
   const [modallsOpen, setlsOpen] = useState(false);
@@ -13,15 +14,18 @@ function Home() {
       right: "auto",
       bottom: "auto",
       marginRight: "-50%",
+      display: "flex",
+      flexDirection: "column",
+      gap: "10px",
       transform: "translate(-50%, -50%)",
     },
   };
 
   function openModal() {
-    setIsOpen(true);
+    setlsOpen(true);
   }
   function closeModal() {
-    setIsOpen(false);
+    setlsOpen(false);
   }
 
   return (
@@ -39,13 +43,28 @@ function Home() {
 
         <div className={styles.rightside}>
           <div className={styles.rightLogo}>
-            <Image src="/twitterLogo.png" alt="Logo" width={80} height={80} />
+            <div className={styles.tLogo}>
+              <Image src="/twitterLogo.png" alt="Logo" width={80} height={80} />
+            </div>
           </div>
           <h1 className={styles.title}>Create your Hackatweet account</h1>
           <h3>Join Hackatweet today</h3>
-          <button onClick={openModal} className={styles.btnLogs}>Sign up</button>
-            <Modal ></Modal>
+
+          <button onClick={openModal} className={styles.btnLogs}>
+            Sign up
+          </button>
           <p>Already have an account ?</p>
+          <Modal
+            isOpen={modallsOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            constentLabel="ExampleModal"
+          >
+            <p className={styles.closX} onClick={closeModal}>X</p>
+            <input placeholder="Username" />
+            <input placeholder="Password" />
+            <button className={styles.btnLogs}>Sign in</button>
+          </Modal>
           <button className={styles.btnLogs}>Sign in</button>
         </div>
       </main>
